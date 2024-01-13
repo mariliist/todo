3dc27a61d0cbcf39e5a70b78c01f4526f2b88020
Mariliis Toon <mariliistoon@gmail.com>
2024-01-13 11:54:37 +0200

    todo rout

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,36 +3,31 @@ const db = require('../db');
 
 const router = express.Router();
 
-router.get('/todo', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const todo = await db.query('SELECT * FROM todo;');  
-        res.status(200).json({todo: data.rows});
-    }
-    catch(error) {
+        const data = await db.query('SELECT * FROM todo;');  
+        res.status(200).json({ todo: data.rows });
+    } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-
 });
 
-router.post('/todo', async (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body);
     const { task } = req.body;
-
     try {
         const data = await db.query('INSERT INTO todo (task) VALUES ($1);', [task]);
-        res.status(200).json({message: `${data.rowCount} row inserted.`});
-    } 
-    catch (error) {
+        res.status(200).json({ message: `${data.rowCount} row inserted.` });
+    } catch (error) {
         console.log(error);
         res.status(500).json({ error: 'Internal Server Error' });
     } 
-
 });
 
-router.delete('/todo', async (req, res) => {
-    const {id} = req;
-    
+router.delete('/', async (req, res) => {
+    const { id } = req;
+
     try {
         const data = await db.query("SELECT * FROM todo WHERE id = $1;", [id]);
 
@@ -48,4 +43,4 @@ router.delete('/todo', async (req, res) => {
     }
 });
 
-module.exports = router; 
+module.exports = router;
